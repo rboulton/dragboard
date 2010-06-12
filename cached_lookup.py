ff4419e7f92687b244bc70691eda7902fc6bb3e8
Richard Boulton <richard@tartarus.org>
2010-06-12 19:40:04 +0100

    Fix encoding problems

--- cached_lookup.py
+++ cached_lookup.py
@@ -8,15 +8,15 @@ class CachedLookup(db.Model):
     mtime = db.DateTimeProperty(auto_now=True)
 
 def geturl(url):
-    results = db.Query(CachedLookup).filter('url =', url).fetch(limit=1)
+    results = db.Query(CachedLookup).filter('url =', url.encode('utf8')).fetch(limit=1)
     if len(results) > 0:
         c = results[0]
         if c.mtime + datetime.timedelta(days=1) < datetime.datetime.now():
             c.delete()
         else:
-            return c.body
+            return c.body.encode('utf8')
 
     body = urllib2.urlopen(url).read()
-    l = CachedLookup(url=url, body=body)
+    l = CachedLookup(url=url.decode('utf8'), body=body.decode('utf8'))
     l.put()
     return body
